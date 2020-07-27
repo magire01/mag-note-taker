8d0d0a641f2a8fa3f04cae21c52efbf6e08277db
Mark Gire <magire01@gmail.com>
2020-07-27 00:21:42 -0400

    Set up basic express server with index and notes html routes

---
+++ Develop/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/Assets/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+
