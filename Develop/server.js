bd27ea7906f0925a16033d65455c1fe9fec846b2
Mark Gire <magire01@gmail.com>
2020-07-27 00:47:32 -0400

    Added api/notes

--- Develop/server.js
+++ Develop/server.js
@@ -13,9 +13,14 @@ app.use(express.json());
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
+    console.log("entered index.html")
 });
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "notes.html"));
+    console.log("entered notes.html");
 });
 
+app.get("/api/notes", function (req, res) {
+    res.json(noteListItems);
+});
