02a9af442a8c647088d6662bb276ef77a0bbc2c5
Mark Gire <magire01@gmail.com>
2020-07-28 03:21:49 -0400

    db.json is now updated, but array is deleted and will not save

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
+
 const PORT = process.env.PORT || 3000;
-
-
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +12,7 @@ app.use(express.static("public"));
 
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname,  "/public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
     console.log("entered index.html")
 });
 
@@ -37,11 +35,13 @@ app.get("/api/notes", function (req, res) {
 
 app.post("/api/notes", function (req, res) {
     const dbNote = req.body;
+    const id = parseInt(req.params.id);
     fs.readFile("db.json", function (err, data) {
         if (err) {
             throw err;
         } else {
-            const noteField = json.parse(data);
+            const noteField = JSON.parse(data);
+            noteField.id = noteField.length - 1;
             noteField.push(dbNote);
         }
     });
@@ -54,6 +54,8 @@ app.post("/api/notes", function (req, res) {
         }
     });
 });
+
+
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
